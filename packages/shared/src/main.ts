5f91815cbb4724fe4a61c17f71f667138a5bcc78
bit <2928318801@qq.com>
2026-05-24 14:04:17 +0800

    feat: 前端依赖构建

--- packages/shared/src/main.ts
+++ packages/shared/src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'dayjs/locale/zh-cn'
+
+import * as ElementIcons from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+app.use(pinia)
+
+app.use(ElementPlus, { locale: zhCn })
+for (const [k, c] of Object.entries(ElementIcons)) {
+  app.component(k, c)
+}
+
+app.mount('#app')
