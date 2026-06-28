ba152684acb80ea6af00712bfc25d100eb6b2b9f
bit <2928318801@qq.com>
2026-06-28 16:24:36 +0800

    feat: fix

--- apps/fe/src/pages/(default)/index.vue
+++ apps/fe/src/pages/(default)/index.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <div class="index">
-    <div v-for="i in 500">HomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHome</div>
-  </div>
+  <div class="index">Home</div>
 </template>
 
 <style lang="scss" scoped>
