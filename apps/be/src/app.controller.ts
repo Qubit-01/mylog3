f66a9234fb4f42f27262b359bd12dc2942ba81f5
bit <liaoshiqiang@kuaishou.com>
2026-06-29 17:31:56 +0800

    feat: 去除 test 写入app

--- apps/be/src/app.controller.ts
+++ apps/be/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  /**
+   * 测试后端服务是否正常（GET）
+   * 访问：GET /hello
+   */
+  @Get('hello')
+  hello(@Body() body: unknown): string {
+    console.log('🐤 Hello World!', body);
+    return `🐤 Hello World! ${JSON.stringify(body)}`;
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  /**
+   * 测试后端服务是否正常（POST）
+   * 访问：POST /hello
+   */
+  @Post('hello')
+  helloPost(@Body() body: unknown): string {
+    console.log('🐤 Hello World!', body);
+    return `🐤 Hello World! ${JSON.stringify(body)}`;
   }
 }
--- apps/be/src/app.module.ts
+++ apps/be/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TestController } from './test/test.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
-  controllers: [TestController, AppController],
-  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
